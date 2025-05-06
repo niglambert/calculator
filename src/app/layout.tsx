a9a0657dd440f25d55858975a80a56d3a5b2f645
niglambert <nig_lambert@yahoo.com>
2025-05-06 09:12:58 +0100

    no drag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased touch-manipulation`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased touch-manipulation no-drag`}
       >
         {children}
       </body>
