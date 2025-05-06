d00ae8bc5cd04f39ac25a0a06212993cee7246be
niglambert <nig_lambert@yahoo.com>
2025-05-06 09:55:20 +0100

    removed header footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import Container from "./components/Container";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
       <Container />
-      <Footer />
     </div>
   );
 }
