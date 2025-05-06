d45668f29af15a5a9987f3e635f52fcded60454d
niglambert <nig_lambert@yahoo.com>
2025-05-06 09:20:16 +0100

    key tap scale

--- src/app/components/CalculatorKey.tsx
+++ src/app/components/CalculatorKey.tsx
@@ -14,7 +14,7 @@ const CalculatorKey = ({
   return (
     <div
       className={cn(
-        "w-full h-16 flex justify-center items-center rounded-md text-5xl font-bold bg-[#7895A8] shadow shadow-gray-500 hover:cursor-pointer hover:opacity-90 active:opacity-95 text-amber-50",
+        "w-full h-16 flex justify-center items-center rounded-md text-5xl font-bold bg-[#7895A8] shadow shadow-gray-500 hover:cursor-pointer hover:opacity-90 active:opacity-95 text-amber-50 transition-transform duration-100 active:scale-120",
         className
       )}
       onClick={onKeyClick}
